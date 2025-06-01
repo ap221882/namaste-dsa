d9b3d49011ec2469eef0e479acbba35b355f055e
ajay <ap221882@gmail.com>
2025-06-01 19:07:10 +0530

    loops

---
+++ assignments/warmUp/loops.ts
@@ -0,0 +1,26 @@
+for (let i = 2; i < 9; i = i + 2) {
+  console.log('====================================');
+  console.log('Namaste world', i);
+  console.log('====================================');
+}
+
+// 2, 4, 6, 8 - value of i's
+
+/** //> FOR LOOP
+ * Before the loop starts, it initialises the variable. 
+ * Before each iteration, the condition is checked with the current value
+ * after conditional fails or passes, the code inside for loop is executed
+ * after executing the code, the value change(increment/decrement) happens
+ */
+
+/** //> WHILE LOOP
+ */
+
+let i = 7;
+
+while (i > 0) {
+  console.log('====================================');
+  console.log('while loop', i);
+  console.log('====================================');
+  i--;
+}

2a95897ce4419ae5198bc6080ca8887afbba10b5
ajay <ap221882@gmail.com>
2025-06-13 20:34:24 +0530

    merge sorted arrays

---
+++ assignments/arrays0/mergeTwoSortedArrays.ts
@@ -0,0 +1,24 @@
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  const nums1Copy = nums1.slice(0, m);
+
+  let x = 0
+  let y = 0
+  for (let i = 0; i < m + n; i++) {
+    /**
+     * //* here we check when x can be assigned
+     * //* 1. either the x is smaller and within bounds
+     * //* 2. or y is out of bounds - which means the y outbounds the n length
+     * //*
+     * //* We need to keep in mind, negative condition for y is - y > n-1 and positive condition for x is x < m
+     */
+    if ((y > n - 1) || ((nums1Copy[x] <= nums2[y]) && (x < m))) {
+      nums1[i] = nums1Copy[x]
+      x++
+    } else {
+      nums1[i] = nums2[y]
+      y++
+    }
+  }
+
+};

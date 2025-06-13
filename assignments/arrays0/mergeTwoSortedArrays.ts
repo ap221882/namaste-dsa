b95baa20adea552225c6fb1600b3e4f0c998f126
ajay <ap221882@gmail.com>
2025-06-13 20:54:23 +0530

    merge without copy

--- assignments/arrays0/mergeTwoSortedArrays.ts
+++ assignments/arrays0/mergeTwoSortedArrays.ts
@@ -22,3 +22,20 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   }
 
 };
+
+
+
+function mergeWithoutCopy(nums1: number[], m: number, nums2: number[], n: number): void {
+  let x = m - 1;
+  let y = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (y < 0 || (x >= 0 && nums1[x] > nums2[y])) {
+      nums1[i] = nums1[x]
+      x--;
+    } else {
+      nums1[i] = nums2[y]
+      y--
+    }
+  }
+};

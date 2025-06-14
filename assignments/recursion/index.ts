b9c6b0410f4f462e6bf02af524e006839a3da363
ajay <ap221882@gmail.com>
2025-06-14 15:30:27 +0530

    notes

--- assignments/recursion/index.ts
+++ assignments/recursion/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello from new DSA assignment!");
+/**
+ * //* Recursion has 2 phases
+ * //* 1. Calling phase
+ * //* 2. returning phase
+ */
+
+
+//~> Depending on the problem, we write the base case after or before the calling phase
